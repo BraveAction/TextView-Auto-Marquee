022e88305378eb06646a9a2d3b18857171a45fa9
Jony Mao <maoxiaofei2009@gmail.com>
2013-04-23 16:23:29 +0800

    add source file

---
+++ src/com/example/textview_paomadeng_01/WTextView.java
@@ -0,0 +1,31 @@
+package com.example.textview_paomadeng_01;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+
+public class WTextView extends TextView {
+
+	public WTextView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	public WTextView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		// TODO Auto-generated constructor stub
+	}
+
+	public WTextView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean isFocused() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
